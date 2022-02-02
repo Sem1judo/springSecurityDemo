c33f6b36ed018bf14983843da715d09ecea12ec8
Semkov Stanislav <34599107+Sem1judo@users.noreply.github.com>
2022-02-02 23:33:18 +0200

    error handler works

--- src/main/java/com/example/springsecuritydemo/controller/ErrorControllerHandler.java
+++ src/main/java/com/example/springsecuritydemo/controller/ErrorControllerHandler.java
@@ -1,27 +1,28 @@
 package com.example.springsecuritydemo.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-public class ErrorController {
 
-    @RequestMapping("/error")
+@Controller
+public class ErrorControllerHandler implements ErrorController {
+
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
 
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error/error-404";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
                 return "error/error-500";
-            }
-            else if(statusCode == HttpStatus.FORBIDDEN.value()) {
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
                 return "error/error-403";
             }
         }
