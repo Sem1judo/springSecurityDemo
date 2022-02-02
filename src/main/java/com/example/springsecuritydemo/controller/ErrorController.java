d4cb96f68cd1df2b9d0e876b8e75bffc57d5eca2
Semkov Stanislav <34599107+Sem1judo@users.noreply.github.com>
2022-02-02 23:08:26 +0200

    edit btn

--- src/main/java/com/example/springsecuritydemo/controller/ErrorController.java
+++ src/main/java/com/example/springsecuritydemo/controller/ErrorController.java
@@ -16,15 +16,15 @@ public class ErrorController {
             int statusCode = Integer.parseInt(status.toString());
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error-404";
+                return "error/error-404";
             }
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500";
+                return "error/error-500";
             }
             else if(statusCode == HttpStatus.FORBIDDEN.value()) {
-                return "error-403";
+                return "error/error-403";
             }
         }
-        return "error";
+        return "error/error";
     }
 }
