bfc4fb729e88ab39d92ebc256598c3ea9f0d2eab
Semkov Stanislav <34599107+Sem1judo@users.noreply.github.com>
2022-02-03 10:40:38 +0200

    login bug try fix

--- src/main/java/com/example/springsecuritydemo/controller/AuthController.java
+++ src/main/java/com/example/springsecuritydemo/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.example.springsecuritydemo.controller;
 
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +9,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/auth")
-@AllArgsConstructor
 public class AuthController {
 
     @PostMapping("/login")
     public ModelAndView getLoginPagePost(@RequestParam(value = "error", defaultValue = "false") boolean loginError) {
 
-        ModelAndView mav = new ModelAndView("/login");
+        ModelAndView mav = new ModelAndView("/user/login");
         if (loginError) {
             mav.addObject("message", "Email or password is incorrect");
             mav.addObject("error", true);
@@ -27,7 +25,7 @@ public class AuthController {
     @GetMapping("/login")
     public ModelAndView getLoginPageGet(@RequestParam(value = "error", defaultValue = "false") boolean loginError) {
 
-        ModelAndView mav = new ModelAndView("/login");
+        ModelAndView mav = new ModelAndView("/user/login");
         if (loginError) {
             mav.addObject("message", "Email or password is incorrect");
             mav.addObject("error", true);
